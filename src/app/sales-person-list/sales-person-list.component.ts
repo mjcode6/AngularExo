69aa7cbb60b20fb7f69cb5fbd8c1e0285e28af3f
sivasuthan manojan <mjcode6@gmail.com>
2024-04-03 10:14:25 +0200

    it's finish

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -4,7 +4,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrl: './sales-person-list.component.css'
 })
 export class SalesPersonListComponent {
@@ -12,14 +12,14 @@ export class SalesPersonListComponent {
   // create array of an object
 
   salesPersonList: SalesPerson[] = [
+    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 50000),
     new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
     new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
+    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 60000),
+    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 45000),
     new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
     new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
-    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
-    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
-    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
-    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 76000),
+    new SalesPerson("manoj","suthan","mjcode6@gmail.com", 20000),
 
   ];
 
